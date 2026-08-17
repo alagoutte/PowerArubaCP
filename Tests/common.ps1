bb59b3e302befebd5db332de9a7a23cbc11a6450
Alexis La Goutte <alexis.lagoutte@gmail.com>
2026-08-17 10:17:38 +0200

    common(tests): remove Version check

--- Tests/common.ps1
+++ Tests/common.ps1
@@ -32,12 +32,6 @@ Connect-ArubaCP @invokeParams
 $script:MySecurePassword = ConvertTo-SecureString MyPassword -AsPlainText -Force
 $script:MyNewSecurePassword = ConvertTo-SecureString MyNewassword -AsPlainText -Force
 
-$script:VersionBefore680 = $DefaultArubaCPConnection.Version -lt [version]"6.8.0"
-$script:VersionBefore686 = $DefaultArubaCPConnection.Version -lt [version]"6.8.6"
-$script:VersionBefore690 = $DefaultArubaCPConnection.Version -lt [version]"6.9.0"
-$script:VersionBefore6100 = $DefaultArubaCPConnection.Version -lt [version]"6.10.0"
-$script:VersionBefore6110 = $DefaultArubaCPConnection.Version -lt [version]"6.11.0"
-
 $script:server_uuid = (Get-ArubaCPServerConfiguration)[0].server_uuid
 
 Disconnect-ArubaCP -confirm:$false
